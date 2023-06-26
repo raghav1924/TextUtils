0e2bf26a1617005b035e76f5ad350390a3d59528
jatinsharma <raghavjatin245@gmail.com>
2023-06-26 16:10:06 +0530

    Video 16 deploment

--- src/App.js
+++ src/App.js
@@ -1,21 +1,63 @@
 // import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 import About from './compnents/About';
 import NavBar from './compnents/NavBar';
 import TextForm from './compnents/TextForm';
+import Alert from './compnents/Alert';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 
 // let name='Jatin';
 function App() {
-  return (
-    <>
-      <NavBar title='TextUtils' aboutText='About-TextUtils'/>
-      {/* <NavBar /> */}
-      <div className="container my-3">
+  const [mode,setMode]= useState('light');
+  const toggleMode=()=>{
+    if(mode==='light'){
+      setMode('dark');
+      document.body.style.backgroundColor='#35333f';
+      document.body.style.color='white';
+      showAlert('Dark Mode Enabled','success');
+      document.title='TextUtils | Dark Mode'
+    }else {
+      setMode('light');
+      document.body.style.backgroundColor='white';
+      document.body.style.color='black';
+      showAlert('Light Mode Enabled','success');
+      document.title='TextUtils | Light Mode'
+      
 
-      <TextForm title='Enter the Text to Analyze'/>
+    }
+  }
+
+  const [alert,setAlert]=useState(null);
+
+  const showAlert=(message,type)=>{
+    setAlert({
+      msg:message,
+      type:type
+    })
+    setTimeout(()=>{
+      setAlert(null)
+    },1500);
+  }
+
+  return (
+    <BrowserRouter>
+    <div >
+      <NavBar title='TextUtils' mode={mode} toggleMode={toggleMode} aboutText='About-TextUtils'/>
+      {/* <NavBar /> */}
+      <Alert alert={alert}/>
+      <div className="container my-3">
+      <Routes>
+        <Route path="/" element={<TextForm showAlert={showAlert} mode={mode} title='Enter the Text to Analyze'/>} />
+        <Route path="/about" element={<About/>} />
+      </Routes>
+      
       {/* <About/> */}
       </div>
-    </>
+    </div>
+    
+    </BrowserRouter>
   );
 }
 
--- src/compnents/TextForm.js
+++ src/compnents/TextForm.js
@@ -4,24 +4,33 @@ export default function TextForm(props) {
     const handleUpChange=()=>{
         // console.log("To UpperCase ");
         setText(text.toUpperCase());
+        props.showAlert('Text Converted to UpperCase','success');
     }
     const handleLowChange=()=>{
         // console.log("To UpperCase ");
         setText(text.toLowerCase());
+        props.showAlert('Text Converted to LowerCase','success');
+
     }
     const handleCleanChange=()=>{
         // console.log("To UpperCase ");
         setText('');
+        props.showAlert('All Cleared','success');
+
     }
 
     const handleCopy=()=>{
         var tx =document.getElementById('myBox');
         tx.select();
         navigator.clipboard.writeText(tx.value);
+        props.showAlert('Text Copied','success');
+
     }
     const handleExtraSpace=()=>{
         let temp=text.split(/[ ]+/);
         setText(temp.join([" "]));
+        props.showAlert('Extra Space Removed','success');
+
     }
     const handleOnChange=(event)=>{
         setText(event.target.value);
@@ -45,11 +54,11 @@ export default function TextForm(props) {
     <div className="container">    
         <h2>{props.title}</h2>
         <div className="mb-3">
-        <textarea placeholder='Enter Your Text Here' className="form-control shadow" value={text} onChange={handleOnChange} id="myBox" rows="10"></textarea>
+        <textarea placeholder='Enter Your Text Here' style={{backgroundColor:props.mode==='light'?'white':'#6a677a' , color:props.mode==='light'?'black':'white'}} className="form-control shadow" value={text} onChange={handleOnChange} id="myBox" rows="10"></textarea>
         <div className="btn btn-primary my-3" onClick={handleUpChange}>Convert to UpperCase</div>
         <div className="btn btn-primary my-3 mx-2" onClick={handleLowChange}>Convert to LowerCase</div>
         <div className="btn btn-danger my-3 mx-2" onClick={handleCleanChange}>Clear All</div>
-        <div className="btn btn-info my-3 mx-2" onClick={handleCopy}>Copy</div>
+        <div className="btn btn-success my-3 mx-2" onClick={handleCopy}>Copy</div>
         <div className="btn btn-secondary my-3 mx-2" onClick={handleExtraSpace}>Remove Extra Space</div>
 </div>
 </div>
@@ -58,7 +67,7 @@ export default function TextForm(props) {
     <p><strong>{wordCount(text)}</strong> Words & <strong>{text.length} </strong>Characters</p>
     <p><strong>{0.008*wordCount(text)}</strong>  Minutes will be required to read this!</p>
     <h2>Preview</h2>
-    <p>{text}</p>
+    <p>{text.length>0?text:'Enter Something in the text feild to see the preview of it !'}</p>
 </div>
 
     </>
